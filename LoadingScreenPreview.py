29bfa0623439acbf6f6b6debc0a9e60cb3d06895
Carter <47575258+carter8@users.noreply.github.com>
2020-01-02 20:00:16 -0600

    refactored code and added more documentation

--- LoadingScreenPreview.py
+++ LoadingScreenPreview.py
@@ -1,6 +1,6 @@
 import API
 
-api_key = 'RGAPI-4ad9bfde-d344-4f8f-b075-f9a9ee1ecdac' # expires in 24hrs, generated at: developer.riotgames.com
+api_key = 'RGAPI-adab60fc-be49-40e9-b32c-39f814ef8f6e' # expires in 24hrs, generated at: developer.riotgames.com
 summoner_name = 'the carter iii'
 max_games = 30 # cannot be larger than ~45
 
@@ -10,12 +10,27 @@ connection = API.RiotAPI(api_key)
 summoner = connection.get_summoner_by_name(summoner_name)
 
 # gets the info for each summoner in the active game
-# usuable once loading screen starts
+# active game information is only available during the loading screen and actual game
 lobby = connection.get_active_games_by_summoner(summoner['id'])
 
+
 def get_game_stats(game_id, account_id):
+    """
+    Get the stats of a given player for a single game
+
+    :param int game_id:     The match ID (also called game ID)
+    :param int account_id:  The account ID
+
+    :returns:  list of player statistics
+                   list indices and values
+                    0:  1 if win, 0 if loss
+                    1:  number of kills
+                    2:  number of deaths
+                    3:  number of assists
+                    4:  1 (number of games played. will always be 1)
+    """
     # want to return win/loss, kills, deaths, assists
-    stats = [0] * 4
+    stats = [0] * 5
     participant_id = 0
     game_stats = connection.get_match_by_id(game_id)
     for player in game_stats['participantIdentities']:
@@ -24,21 +39,59 @@ def get_game_stats(game_id, account_id):
             break
     for player in game_stats['participants']:
         if player['participantId'] == participant_id:
-            if player['stats']['win'] == True:
+            if player['stats']['win']:
                 stats[0] = 1
             stats[1] += player['stats']['kills']
             stats[2] += player['stats']['deaths']
             stats[3] += player['stats']['assists']
+            stats[4] = 1
             break
     return stats
 
-# info for summoner in index 0 of active game
-summoner_1 = [''] * 4
-summoner_1[0] = lobby['participants'][0]['summonerName']
-summoner_1[1] = lobby['participants'][0]['summonerId']
-summoner_1[2] = (connection.get_summoner_by_name(summoner_1[0]))['accountId']
-summoner_1[3] = lobby['participants'][0]['championId']
 
+def add_game_stats(total_stats, stats_to_add):
+    """
+    Adds a list of game stats to another list of game stats
+
+    :param list total_stats:   The list of total stats that will get the other list added to it
+    :param list stats_to_add:  The list of game stats that are being added
+    """
+    total_stats[0] += stats_to_add[0]
+    total_stats[1] += stats_to_add[1]
+    total_stats[2] += stats_to_add[2]
+    total_stats[3] += stats_to_add[3]
+    total_stats[4] += stats_to_add[4]
+
+
+def print_stats(stats, name):
+    """
+    Prints the win rate and kill/death ratio for a list of player stats
+
+    :param list stats:   The list of player stats to that are being summarized and printed
+    :param string name:  The name of the player
+    """
+    stats_to_print = stats
+    stats_to_print.append(stats[4] - stats[0])
+    stats_to_print[1] = stats_to_print[1] / stats_to_print[4]  # avg kills
+    stats_to_print[2] = stats_to_print[2] / stats_to_print[4]  # avg deaths
+    stats_to_print[3] = stats_to_print[3] / stats_to_print[4]  # avg assists
+    print('%s on Selected Champion:' % name)
+    print('Winrate: %.0f%% (%dW / %dL)' % (stats_to_print[0]/stats_to_print[4]*100, stats_to_print[0], stats_to_print[5]))
+    print('Average KDA: %.2f' % (stats_to_print[1]/stats_to_print[2]))
+    print()
+
+
+def get_summoner_from_active_game(active_game, summoner_index, api_connection):
+    summoner_info = [''] * 4
+    summoner_info[0] = active_game['participants'][summoner_index]['summonerName']
+    summoner_info[1] = active_game['participants'][summoner_index]['summonerId']
+    summoner_info[2] = (api_connection.get_summoner_by_name(summoner_info[0]))['accountId']
+    summoner_info[3] = active_game['participants'][summoner_index]['championId']
+    return summoner_info
+
+
+# info for summoner in index 0 of active game
+summoner_1 = get_summoner_from_active_game(lobby, 0, connection)
 summoner_1_matchlist = connection.get_matchlist_by_account(summoner_1[2], summoner_1[3])
 summoner_1_stats = [0] * 5
 total_games = 0
@@ -52,28 +105,11 @@ else:
         if total_games > max_games:
             break
         game = get_game_stats(match['gameId'], summoner_1[2])
-        summoner_1_stats[4] += 1
-        summoner_1_stats[0] = game[0]
-        summoner_1_stats[1] = game[1]
-        summoner_1_stats[2] = game[2]
-        summoner_1_stats[3] = game[3]
-    
-summoner_1_stats.append(summoner_1_stats[4] - summoner_1_stats[0]) # add number of losses
-summoner_1_stats[1] = summoner_1_stats[1] / summoner_1_stats[4] # avg kills
-summoner_1_stats[2] = summoner_1_stats[2] / summoner_1_stats[4] # avg deaths
-summoner_1_stats[3] = summoner_1_stats[3] / summoner_1_stats[4] # avg assists
-print('%s on Selected Champion' % (summoner_1[0]))
-print('Winrate: %.0f%% (%dW / %dL)' % (summoner_1_stats[0]/summoner_1_stats[4]*100,summoner_1_stats[0], summoner_1_stats[5]))
-print('Average KDA: %.2f' % (summoner_1_stats[1]/summoner_1_stats[2]))
-print()
+        add_game_stats(summoner_1_stats, game)
+    print_stats(summoner_1_stats, summoner_1[0])
 
 # info for summoner in index 5 of active game
-summoner_2 = [''] * 4
-summoner_2[0] = lobby['participants'][5]['summonerName']
-summoner_2[1] = lobby['participants'][5]['summonerId']
-summoner_2[2] = (connection.get_summoner_by_name(summoner_1[0]))['accountId']
-summoner_2[3] = lobby['participants'][5]['championId']
-
+summoner_2 = get_summoner_from_active_game(lobby, 6, connection)
 summoner_2_matchlist = connection.get_matchlist_by_account(summoner_2[2], summoner_2[3])
 summoner_2_stats = [0] * 5
 total_games = 0
@@ -86,16 +122,5 @@ else:
         if total_games > max_games:
             break
         game = get_game_stats(match['gameId'], summoner_2[2])
-        summoner_1_stats[4] += 1
-        summoner_1_stats[0] = game[0]
-        summoner_1_stats[1] = game[1]
-        summoner_1_stats[2] = game[2]
-        summoner_1_stats[3] = game[3]
-    
-summoner_2_stats.append(summoner_2_stats[4] - summoner_2_stats[0]) # add number of losses
-summoner_2_stats[1] = summoner_2_stats[1] / summoner_2_stats[4] # avg kills
-summoner_2_stats[2] = summoner_2_stats[2] / summoner_2_stats[4] # avg deaths
-summoner_2_stats[3] = summoner_2_stats[3] / summoner_2_stats[4] # avg assists
-print('%s on Selected Champion' % (summoner_2[0]))
-print('Winrate: %.0f%% (%dW / %dL)' % (summoner_2_stats[0]/summoner_2_stats[4]*100,summoner_2_stats[0], summoner_2_stats[5]))
-print('Average KDA: %.2f' % (summoner_2_stats[1]/summoner_2_stats[2]))
+        add_game_stats(summoner_2_stats, game)
+    print_stats(summoner_2_stats, summoner_2[0])
--- SummonerLookup.py
+++ SummonerLookup.py
@@ -1,195 +1,168 @@
+"""
+This script gives the win rate and kill/death ratio a player in each different
+game mode they have played recently in League of Legends, as well as a total
+win rate and kill/death ratio.
+
+To look up the stats for a different player, simply change the summoner name
+"""
+
 import API
 
-# gives win rate and KDA for each queue
-
-api_key = 'RGAPI-8c9357f7-5447-451b-9da0-3b33922a97ad' # expires in 24hrs, generated at: developer.riotgames.com
-summoner_name = 'the carter iii'
-max_games = 90 # cannot be larger than 98
+api_key = 'RGAPI-adab60fc-be49-40e9-b32c-39f814ef8f6e' # expires in 24hrs, generated at: developer.riotgames.com
+summoner_name = 'xo slime'
+max_games = 90 # cannot be larger than 98 due to rate limit on requests to the Riot API
 
 connection = API.RiotAPI(api_key)
 
 # get summoner info for desired name
 summoner = connection.get_summoner_by_name(summoner_name)
 
-matchlist = connection.get_matchlist_by_account(summoner['accountId'])
+# get the most recent matches for the summoner
+match_list = connection.get_matchlist_by_account(summoner['accountId'])
+
 
 def get_game_stats(game_id, account_id):
-	# want to return win/loss, kills, deaths, assists
-	stats = [0] * 4
-	participant_id = 0
-	game_stats = connection.get_match_by_id(game_id)
-	for player in game_stats['participantIdentities']:
-		if player['player']['accountId'] == account_id:
-			participant_id = player['participantId']
-			break
-	for player in game_stats['participants']:
-		if player['participantId'] == participant_id:
-			if player['stats']['win'] == True:
-				stats[0] = 1
-			stats[1] += player['stats']['kills']
-			stats[2] += player['stats']['deaths']
-			stats[3] += player['stats']['assists']
-			break
-	return stats
-	
+    """
+    Get the stats of a given player for a single game
 
-# iterate through matchlist
-# limiting to 50 matches. api key has a rate limit of 100 requests every 2 mins
+    :param int game_id:     The match ID (also called game ID)
+    :param int account_id:  The account ID
+
+    :returns:  list of player statistics
+                   list indices and values
+                    0:  1 if win, 0 if loss
+                    1:  number of kills
+                    2:  number of deaths
+                    3:  number of assists
+                    4:  1 (number of games played. will always be 1)
+    """
+    # want to return win/loss, kills, deaths, assists
+    stats = [0] * 5
+    participant_id = 0
+    game_stats = connection.get_match_by_id(game_id)
+    for player in game_stats['participantIdentities']:
+        if player['player']['accountId'] == account_id:
+            participant_id = player['participantId']
+            break
+    for player in game_stats['participants']:
+        if player['participantId'] == participant_id:
+            if player['stats']['win']:
+                stats[0] = 1
+            stats[1] += player['stats']['kills']
+            stats[2] += player['stats']['deaths']
+            stats[3] += player['stats']['assists']
+            stats[4] = 1
+            break
+    return stats
+
+
+def add_game_stats(total_stats, stats_to_add):
+    """
+    Adds a list of game stats to another list of game stats
+
+    :param list total_stats:   The list of total stats that will get the other list added to it
+    :param list stats_to_add:  The list of game stats that are being added
+    """
+    total_stats[0] += stats_to_add[0]
+    total_stats[1] += stats_to_add[1]
+    total_stats[2] += stats_to_add[2]
+    total_stats[3] += stats_to_add[3]
+    total_stats[4] += stats_to_add[4]
+
+
+def print_stats(stats, queue):
+    """
+    Prints the win rate and kill/death ratio for a list of player stats
+
+    :param list stats:    The list of player stats to that are being summarized and printed
+    :param string queue:  The name of the queue where the stats are from
+    """
+    stats_to_print = stats
+    stats_to_print.append(stats[4] - stats[0])
+    stats_to_print[1] = stats_to_print[1] / stats_to_print[4]  # avg kills
+    stats_to_print[2] = stats_to_print[2] / stats_to_print[4]  # avg deaths
+    stats_to_print[3] = stats_to_print[3] / stats_to_print[4]  # avg assists
+    print('%s | Winrate: %.0f%% (%dW / %dL)' % (queue, stats_to_print[0]/stats_to_print[4]*100, stats_to_print[0], stats_to_print[5]))
+    print('Average KDA: %.2f' % (stats_to_print[1]/stats_to_print[2]))
+    print()
+
+
+# iterate through match list and get stats for each queue
 urf_stats = [0] * 5
 aram_stats = [0] * 5
+pk_stats = [0] * 5
 blind_stats = [0] * 5
 draft_stats = [0] * 5
 ranked_stats = [0] * 5
 ranked_flex_stats = [0] * 5
 total_games = 0
-for match in matchlist['matches']:
-	total_games += 1
-	if total_games > max_games:
-		break
-	if match['queue'] == 430:
-		# unranked blind pick
-		blind = get_game_stats(match['gameId'], summoner['accountId'])
-		blind_stats[4] += 1
-		blind_stats[0] += blind[0]
-		blind_stats[1] += blind[1]
-		blind_stats[2] += blind[2]
-		blind_stats[3] += blind[3]
-	elif match['queue'] == 450:
-		# ARAM
-		aram = get_game_stats(match['gameId'], summoner['accountId'])
-		aram_stats[4] += 1
-		aram_stats[0] += aram[0]
-		aram_stats[1] += aram[1]
-		aram_stats[2] += aram[2]
-		aram_stats[3] += aram[3]
-	elif match['queue'] == 400:
-		# unranked draft pick
-		draft = get_game_stats(match['gameId'], summoner['accountId'])
-		draft_stats[4] += 1
-		draft_stats[0] += draft[0]
-		draft_stats[1] += draft[1]
-		draft_stats[2] += draft[2]
-		draft_stats[3] += draft[3]
-	elif match['queue'] == 420:
-		# ranked solo/duo pick
-		ranked = get_game_stats(match['gameId'], summoner['accountId'])
-		ranked_stats[4] += 1
-		ranked_stats[0] += ranked[0]
-		ranked_stats[1] += ranked[1]
-		ranked_stats[2] += ranked[2]
-		ranked_stats[3] += ranked[3]
-	elif match['queue'] == 440:
-		# ranked flex pick
-		ranked_flex = get_game_stats(match['gameId'], summoner['accountId'])
-		ranked_flex_stats[4] += 1
-		ranked_flex_stats[0] += ranked_flex[0]
-		ranked_flex_stats[1] += ranked_flex[1]
-		ranked_flex_stats[2] += ranked_flex[2]
-		ranked_flex_stats[3] += ranked_flex[3]
-	elif match['queue'] == 900:
-		# URF
-		urf = get_game_stats(match['gameId'], summoner['accountId'])
-		urf_stats[4] += 1
-		urf_stats[0] += urf[0]
-		urf_stats[1] += urf[1]
-		urf_stats[2] += urf[2]
-		urf_stats[3] += urf[3]
-	else:
-		print('Queue not coded. Queue #: %d' % match['queue'])
-		
-		
+for match in match_list['matches']:
+    total_games += 1
+    if total_games < max_games:
+        if match['queue'] == 430:
+            # unranked blind pick
+            blind_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(blind_stats, blind_game)
+        elif match['queue'] == 920:
+            # poro king
+            pk_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(pk_stats, pk_game)
+        elif match['queue'] == 450:
+            # ARAM
+            aram_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(aram_stats, aram_game)
+        elif match['queue'] == 400:
+            # unranked draft pick
+            draft_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(draft_stats, draft_game)
+        elif match['queue'] == 420:
+            # ranked solo/duo pick
+            ranked_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(ranked_stats, ranked_game)
+        elif match['queue'] == 440:
+            # ranked flex pick
+            ranked_flex_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(ranked_flex_stats, ranked_flex_game)
+        elif match['queue'] == 900:
+            # URF
+            urf_game = get_game_stats(match['gameId'], summoner['accountId'])
+            add_game_stats(urf_stats, urf_game)
+        else:
+            print('Queue not coded. Queue #: %d' % match['queue'])
+    else:
+        break
+
 # calculate total stats and print queue summaries
 total_stats = [0] * 5
 
 print('Stats for %s:\n' % (summoner['name']))
 
 if urf_stats[4] != 0:
-	total_stats[0] += urf_stats[0]
-	total_stats[1] += urf_stats[1]
-	total_stats[2] += urf_stats[2]
-	total_stats[3] += urf_stats[3]
-	total_stats[4] += urf_stats[4]
-	urf_stats.append(urf_stats[4] - urf_stats[0]) # add number of losses
-	urf_stats[1] = urf_stats[1] / urf_stats[4] # avg kills
-	urf_stats[2] = urf_stats[2] / urf_stats[4] # avg deaths
-	urf_stats[3] = urf_stats[3] / urf_stats[4] # avg assists
-	print('URF | Winrate: %.0f%% (%dW / %dL)' % (urf_stats[0]/urf_stats[4]*100,urf_stats[0], urf_stats[5]))
-	print('Average KDA: %.2f' % (urf_stats[1]/urf_stats[2]))
-	print()
-	
-if aram_stats[4] != 0:
-	total_stats[0] += aram_stats[0]
-	total_stats[1] += aram_stats[1]
-	total_stats[2] += aram_stats[2]
-	total_stats[3] += aram_stats[3]
-	total_stats[4] += aram_stats[4]
-	aram_stats.append(aram_stats[4] - aram_stats[0]) # add number of losses
-	aram_stats[1] = aram_stats[1] / aram_stats[4] # avg kills
-	aram_stats[2] = aram_stats[2] / aram_stats[4] # avg deaths
-	aram_stats[3] = aram_stats[3] / aram_stats[4] # avg assists
-	print('ARAM | Winrate: %.0f%% (%dW / %dL)' % (aram_stats[0]/aram_stats[4]*100,aram_stats[0], aram_stats[5]))
-	print('Average KDA: %.2f' % (aram_stats[1]/aram_stats[2]))
-	print()
-	
-if blind_stats[4] != 0:
-	total_stats[0] += blind_stats[0]
-	total_stats[1] += blind_stats[1]
-	total_stats[2] += blind_stats[2]
-	total_stats[3] += blind_stats[3]
-	total_stats[4] += blind_stats[4]
-	blind_stats.append(blind_stats[4] - blind_stats[0]) # add number of losses
-	blind_stats[1] = blind_stats[1] / blind_stats[4] # avg kills
-	blind_stats[2] = blind_stats[2] / blind_stats[4] # avg deaths
-	blind_stats[3] = blind_stats[3] / blind_stats[4] # avg assists
-	print('Blind Pick | Winrate: %.0f%% (%dW / %dL)' % (blind_stats[0]/blind_stats[4]*100,blind_stats[0], blind_stats[5]))
-	print('Average KDA: %.2f' % (blind_stats[1]/blind_stats[2]))
-	print()
-	
-if draft_stats[4] != 0:
-	total_stats[0] += draft_stats[0]
-	total_stats[1] += draft_stats[1]
-	total_stats[2] += draft_stats[2]
-	total_stats[3] += draft_stats[3]
-	total_stats[4] += draft_stats[4]
-	draft_stats.append(draft_stats[4] - draft_stats[0]) # add number of losses
-	draft_stats[1] = draft_stats[1] / draft_stats[4] # avg kills
-	draft_stats[2] = draft_stats[2] / draft_stats[4] # avg deaths
-	draft_stats[3] = draft_stats[3] / draft_stats[4] # avg assists
-	print('Draft Pick | Winrate: %.0f%% (%dW / %dL)' % (draft_stats[0]/draft_stats[4]*100,draft_stats[0], draft_stats[5]))
-	print('Average KDA: %.2f' % (draft_stats[1]/draft_stats[2]))
-	print()
-	
-if ranked_flex_stats[4] != 0:
-	total_stats[0] += ranked_flex_stats[0]
-	total_stats[1] += ranked_flex_stats[1]
-	total_stats[2] += ranked_flex_stats[2]
-	total_stats[3] += ranked_flex_stats[3]
-	total_stats[4] += ranked_flex_stats[4]
-	ranked_flex_stats.append(ranked_flex_stats[4] - ranked_flex_stats[0]) # add number of losses
-	ranked_flex_stats[1] = ranked_flex_stats[1] / ranked_flex_stats[4] # avg kills
-	ranked_flex_stats[2] = ranked_flex_stats[2] / ranked_flex_stats[4] # avg deaths
-	ranked_flex_stats[3] = ranked_flex_stats[3] / ranked_flex_stats[4] # avg assists
-	print('Ranked Flex | Winrate: %.0f%% (%dW / %dL)' % (ranked_flex_stats[0]/ranked_flex_stats[4]*100,ranked_flex_stats[0], ranked_flex_stats[5]))
-	print('Average KDA: %.2f' % (ranked_flex_stats[1]/ranked_flex_stats[2]))
-	print()
-	
-if ranked_stats[4] != 0:
-	total_stats[0] += ranked_stats[0]
-	total_stats[1] += ranked_stats[1]
-	total_stats[2] += ranked_stats[2]
-	total_stats[3] += ranked_stats[3]
-	total_stats[4] += ranked_stats[4]
-	ranked_stats.append(ranked_stats[4] - ranked_stats[0]) # add number of losses
-	ranked_stats[1] = ranked_stats[1] / ranked_stats[4] # avg kills
-	ranked_stats[2] = ranked_stats[2] / ranked_stats[4] # avg deaths
-	ranked_stats[3] = ranked_stats[3] / ranked_stats[4] # avg assists
-	print('Ranked Solo/Duo | Winrate: %.0f%% (%dW / %dL)' % (ranked_stats[0]/ranked_stats[4]*100,ranked_stats[0], ranked_stats[5]))
-	print('Average KDA: %.2f' % (ranked_stats[1]/ranked_stats[2]))
-	print()
+    add_game_stats(total_stats, urf_stats)
+    print_stats(urf_stats, 'URF')
 
-total_stats.append(total_stats[4] - total_stats[0]) # add number of losses
-total_stats[1] = total_stats[1] / total_stats[4] # avg kills
-total_stats[2] = total_stats[2] / total_stats[4] # avg deaths
-total_stats[3] = total_stats[3] / total_stats[4] # avg assists
-print('Total Winrate: %.0f%% (%dW / %dL)' % (total_stats[0]/total_stats[4]*100,total_stats[0], total_stats[5]))
-print('Average KDA: %.2f' % (total_stats[1]/total_stats[2]))
+if pk_stats[4] != 0:
+    add_game_stats(total_stats, pk_stats)
+    print_stats(pk_stats, 'Poro King')
+
+if aram_stats[4] != 0:
+    add_game_stats(total_stats, aram_stats)
+    print_stats(aram_stats, 'ARAM')
+
+if blind_stats[4] != 0:
+    add_game_stats(total_stats, blind_stats)
+    print_stats(blind_stats, 'Blind Pick')
+
+if draft_stats[4] != 0:
+    add_game_stats(total_stats, draft_stats)
+    print_stats(draft_stats, 'Draft Pick')
+
+if ranked_flex_stats[4] != 0:
+    add_game_stats(total_stats, ranked_flex_stats)
+    print_stats(ranked_flex_stats, 'Ranked Flex')
+
+if ranked_stats[4] != 0:
+    add_game_stats(total_stats, ranked_stats)
+    print_stats(ranked_stats, 'Ranked Solo/Duo')
+
+print_stats(total_stats, 'Total')
